1117c60e19d96820bae56b799f54547f2ca9f41a
ihebchagra <ihebchagra@gmail.com>
2019-07-09 18:01:36 +0100

    created repo

--- default.nix
+++ default.nix
@@ -14,7 +14,7 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  mutt-wizard = pkgs.callPackage ./pkgs/mutt-wizard { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }

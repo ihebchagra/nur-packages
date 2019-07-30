58cdfacc2dacdea3c3aa33bf0fb3309fe04e2c43
ihebchagra <51883835+ihebchagra@users.noreply.github.com>
2019-07-30 11:23:55 +0100

    Update default.nix

--- pkgs/nudoku/default.nix
+++ pkgs/nudoku/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation {
   name = "nudoku";
-  version = "1.0.0"
+  version = "1.0.0";
   src = fetchurl {
     url = https://github.com/jubalh/nudoku/releases/download/1.0.0/nudoku-1.0.0.tar.xz;
     sha256 = "0nr2j2z07nxk70s8xnmmpzccxicf7kn5mbwby2kg6aq8paarjm8k";

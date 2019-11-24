d9b2f825e08be1d06c1d56bc350dd2476b4e92be
ihebchagra <ihebchagra@gmail.com>
2019-11-24 09:23:55 +0100

    ripcord: init @ 0.4.20 fix

--- default.nix
+++ default.nix
@@ -9,6 +9,6 @@
   nudoku = pkgs.callPackage ./pkgs/nudoku { };
   bemenu = pkgs.callPackage ./pkgs/bemenu { };
   cboard = pkgs.callPackage ./pkgs/cboard { };
-  ripcord = pkgs.callPackage ./pkgs/ripcord/ { };
+  ripcord = pkgs.callPackage ./pkgs/ripcord { };
 }
 
